f2f64f231452b87f5883a7513bfa88b9ddfbe2a9
smsguy927 <smsguy927@gmail.com>
2021-05-18 16:06:10 -0500

    commit tree 249491006a6cc457d664d5e95d2e1f5147991d0f

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+  return arr.slice(0,2)
+}
+
+function returnLastTwoDrivers(arr) {
+  return arr.slice(arr.length - 2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num) {
+  return function(fare) {
+    return fare * num
+  }
+}
+
+function fareDoubler(fare) {
+  return fare * 2;
+}
+
+function fareTripler(fare) {
+  return fare * 3;
+}
+
+function selectDifferentDrivers(arr, func) {
+  return func(arr);
+}
